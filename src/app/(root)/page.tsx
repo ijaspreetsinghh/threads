195ed7182a2d3a9c3d8c033d2e605a5d75b909c3
Jaspreet Singh <44422128+ijaspreetsinghh@users.noreply.github.com>
2024-02-07 21:55:55 +0530

    added post thread added view thread on home page added view thread details added comment to thread [ reply ]

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,37 @@
+import ThreadCard from "@/components/cards/ThreadCard";
+import { fetchThreads } from "@/lib/actions/thread.action";
 import { UserButton } from "@clerk/nextjs";
-
-export default function Home() {
+import { currentUser } from "@clerk/nextjs";
+export default async function Home() {
+  const results = await fetchThreads(1, 30);
+  const user = await currentUser();
   return (
     <div className='h-screen'>
-      <UserButton afterSignOutUrl='/' />
+      <h1 className='head-text text-left'>Home</h1>
+      <section className='mt-9 flex flex-col gap-10'>
+        {results.posts.length === 0 ? (
+          <p className='no-result'>No threads found</p>
+        ) : (
+          <>
+            {results.posts.map((post) => (
+              <ThreadCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user?.id || ""}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
     </div>
   );
+}
+{
+  /* <UserButton afterSignOutUrl='/' /> */
 }
